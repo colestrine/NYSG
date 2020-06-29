bf6fe1364389f8683b8f2c92d47447e204eb6df3
cole <cole.strine@empeq.co>
2020-06-29 13:43:10 -0400

    Ran updated simulation, added comments to RL script

--- Machine Learning/reinforcement_learning.py
+++ Machine Learning/reinforcement_learning.py
@@ -2,32 +2,39 @@ from numpy import random, mean
 from matplotlib import pyplot
 
 class State:
+	# State class holds data for temperature, humidity, soil moisture, and sunlight
 	def __init__(self, temperature, humidity, soil_moisture, sunlight):
 		self.temperature = temperature
 		self.humidity = humidity
 		self.soil_moisture = soil_moisture
 		self.sunlight = sunlight
 
+	# When passed as a string, return data for each dimension
 	def __str__(self):
 		return f"({self.temperature},{self.humidity},{self.soil_moisture},{self.sunlight})"
 
+	# When suntraction operator used, return tuple containing self.data-other.data values
 	def __sub__(self, other):
 		return (self.temperature - other.temperature, self.humidity - other.humidity, self.soil_moisture - other.soil_moisture, self.sunlight - other.sunlight)
 
+	# When != operator used, return truth value for eqality of all dimensions
 	def __ne__(self, other):
 		if ((self.temperature == other.temperature) and (self.humidity == other.humidity) and (self.soil_moisture == other.soil_moisture) and (self.sunlight == other.sunlight)):
 			return False
 		else:
 			return True
 
+	# Return uniformly weighted total distance between self's variables and other's variables
 	def distance(self, other):
 		return abs(self.temperature - other.temperature) + abs(self.humidity - other.humidity) + abs(self.soil_moisture - other.soil_moisture) + abs(self.sunlight - other.sunlight)
 
 class Environment:
+	# Return reward for agent's actions - incorporate considerations for: movement towards goal state, close proximity to goal state, overall distance from goal state
 	def reward(last_state, next_state, goal_state):
 		last_difference = State.distance(last_state, goal_state)
 		next_difference = State.distance(next_state, goal_state)
 
+		# Encourage movement towards goal state
 		if last_difference > next_difference:
 			reward = 500
 		elif last_difference < next_difference:
@@ -35,6 +42,7 @@ class Environment:
 		else:
 			reward = 0
 
+		# Encourage maintaining close proximity to goal state
 		if next_difference < 1:
 			reward += 2000
 		elif next_difference < 2:
@@ -44,10 +52,12 @@ class Environment:
 		else:
 			reward += -100
 
+		# Scaled portion of reward based on distance from goal state
 		reward -= next_difference * 10
 
 		return reward
 
+	# Take current state and actions, return next state based on model of system
 	def transition(current_state, light_action, water_action, ventilation_action):
 		next_state = State(current_state.temperature, current_state.humidity, current_state.soil_moisture, current_state.sunlight)
 
@@ -129,12 +139,14 @@ class Environment:
 		return next_state
 
 class Agent:
+	# Run RL algorithm
 	def run(initial_state, goal_state):
 		if initial_state == goal_state:
 			return ({'light_action': light_action, 'water_action': water_action, 'ventilation_action': ventilation_action}, None)
 
 		action_choices = ['increase', 'decrease', 'none']
 
+		# Iterate through all possible action vectors
 		avg_episode_rewards = []
 		for initial_light_action in action_choices:
 				for initial_water_action in action_choices:
@@ -168,14 +180,10 @@ class Agent:
 											if next_reward > max_next_reward['reward']:
 												max_next_reward = {'reward': next_reward, 'light_action': light_action, 'water_action': water_action, 'ventilation_action': ventilation_action}
 
+								# Extract actions from return of greedy policy
 								light_action = max_next_reward['light_action']
 								water_action = max_next_reward['water_action']
 								ventilation_action = max_next_reward['ventilation_action']
-
-								# # Pick a random action
-								# light_action = action_choices[random.randint(0, len(action_choices))]
-								# water_action = action_choices[random.randint(0, len(action_choices))]
-								# ventilation_action = action_choices[random.randint(0, len(action_choices))]
 
 								# Look one step ahead, observe new state and reward
 								next_state = Environment.transition(current_state, light_action, water_action, ventilation_action)
@@ -192,15 +200,18 @@ class Agent:
 
 							episode_rewards.append(total_reward)
 
+						# Find average of episode rewards, save data
 						avg_episode_reward = mean(episode_rewards)
 						avg_episode_rewards.append({'avg_episode_reward': avg_episode_reward, 'light_action': initial_light_action, 'water_action': initial_water_action, 'ventilation_action': initial_ventilation_action})
 
+		# Find largest average episode reward, the actions used in these episodes are our choice actions
 		avg_episode_rewards = sorted(avg_episode_rewards, key=lambda k: k['avg_episode_reward'], reverse=True)
 		choice = avg_episode_rewards[0]
 
 		return {'light_action': choice['light_action'], 'water_action':  choice['water_action'], 'ventilation_action':  choice['ventilation_action'], 'avg_episode_rewards': avg_episode_rewards}
 
 class Test:
+	# Run simulation test
 	def run(current_state, goal_state, timesteps):
 		# Start at timestep 0
 		timestep = 0
@@ -240,6 +251,7 @@ class Test:
 
 		return {'current_states': current_states, 'goal_states': goal_states}
 
+	# Plot simulation data
 	def plot(current_states, goal_states):
 		if len(current_states) == len(goal_states):
 			length = len(current_states)
