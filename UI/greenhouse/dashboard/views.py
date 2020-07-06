b3093b335d37434413707f4f59a46781186d8ba7
cole <cole.strine@empeq.co>
2020-07-06 14:36:20 -0400

    Added functional Current System Status section on UI/Dashboard

--- UI/greenhouse/dashboard/views.py
+++ UI/greenhouse/dashboard/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from scripts.data_handler import data_handler
+import json
 
 # Create your views here.
 def index(request):
-	return render(request, 'Dashboard/dashboard.html')
+	healthy_levels = data_handler.read_healthy_levels()
+	healthy_temperature = healthy_levels['temperature']
+	healthy_humidity = healthy_levels['humidity']
+	healthy_soil_moisture = healthy_levels['soil_moisture']
+	healthy_sunlight = healthy_levels['sunlight']
+
+	return render(request, 'Dashboard/dashboard.html', {'healthy_temperature': healthy_temperature, 'healthy_humidity': healthy_humidity, 'healthy_soil_moisture': healthy_soil_moisture, 'healthy_sunlight': healthy_sunlight})
