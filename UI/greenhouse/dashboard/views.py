01930e8b994f480816c3b0e940ae91c8f9bbcbf9
cole <cole.strine@empeq.co>
2020-07-13 12:17:29 -0400

    Current levels now driven by log.json

--- UI/greenhouse/dashboard/views.py
+++ UI/greenhouse/dashboard/views.py
@@ -50,4 +50,10 @@ def index(request):
 		soil_moistures += str(log_data[record]['soil_moisture']) + ','
 		sunlights += str(log_data[record]['sunlight']) + ','
 
+	temperatures = temperatures[:-1]
+	humidities = humidities[:-1]
+	soil_moistures = soil_moistures[:-1]
+	sunlights = sunlights[:-1]
+	labels = labels[:-1]
+
 	return render(request, 'Dashboard/dashboard.html', {'healthy_temperature': healthy_temperature, 'healthy_temperature_label': healthy_temperature_label, 'healthy_humidity': healthy_humidity, 'healthy_humidity_label': healthy_humidity_label, 'healthy_soil_moisture': healthy_soil_moisture, 'healthy_soil_moisture_label': healthy_soil_moisture_label, 'healthy_sunlight': healthy_sunlight, 'healthy_sunlight_label': healthy_sunlight_label, 'labels': labels, 'temperatures': temperatures, 'humidities': humidities, 'soil_moistures': soil_moistures, 'sunlights': sunlights})
