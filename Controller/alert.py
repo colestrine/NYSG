611209e7957c4a354cbde39c1a0c584e2c98cf35
Jonathan Tran <jtran2718@gmail.com>
2020-07-16 13:02:03 -0700

    add alert json test logging

--- Controller/alert.py
+++ Controller/alert.py
@@ -3,14 +3,24 @@ alert deals with possible alert messages
 """
 
 # ---------- IMPORTS ----------------------
+import random  # for testing
+import datetime
+import time
 
-from log import log
+# --------- CUSTOM IMPORTS ---------------
+import pin_constants
+from log import init_log, log, MAX_SIZE
 
 
 # --------- CONSTANTS FOR ALERTS ---------
 
 WATER_LEVEL = 100
 ALERT_LOG_PATH = "alert_log.json"
+N_TEST_ITER = 100
+
+# --------- TEST RUNNER ENVIRONMENT VARS --------
+RUN_TEST = False
+
 
 # --------- HELPERS ---------------------
 
@@ -22,15 +32,55 @@ def alert_message_generator():
     """
     return "Water Level is Low"
 
+
+def log_alert(alert_message_dict, alert_path=ALERT_LOG_PATH):
+    """
+    log_alert(alert_message_dict) logs an alert_message_Dict
+    """
+    log(ALERT_LOG_PATH, alert_message_dict, MAX_SIZE)
+
+
 # ---------- MAIN -----------------------
 
 
-def alert(water_level):
+def alert(water_level, alert_path=ALERT_LOG_PATH):
     """
     alert(water_level) raises an alert based on the current water level
-    RETURNS None if no alerts raises
+    logs alert
+    RETURNS None i
     """
+    # get time
+    now = datetime.datetime.now()
+    # add second and microsecond
+    current_time = now.strftime("%d-%m-%Y %H:%M:%-S:%f")
+    time_dict = {}
+
+    alert_dict = {}
     if water_level < WATER_LEVEL:
-        return alert_message_generator()
+        alert_dict["water_level"] = alert_message_generator()
     else:
-        return None
+        alert_dict["water_level"] = None
+
+    time_dict[current_time] = alert_dict
+    log_alert(time_dict, alert_path)
+    return
+
+# ----------- DEBUGGING ----------------
+
+
+def check_alert_works(n_iter, log_path):
+    """
+    check_alert_works(n_iter) checks that alerts are processed and logged properly
+    over n_iters at log_path to log data
+    """
+    for _ in range(n_iter):
+        alert(random.randint(0, WATER_LEVEL * 2), log_path)
+
+
+# ----------- MAIN (FOR DEBUGGING) -----------------
+
+if __name__ == "__main__":
+    if RUN_TEST:
+        # for debugging whether alert logs properly
+        init_log(ALERT_LOG_PATH)
+        check_alert_works(N_TEST_ITER, ALERT_LOG_PATH)
