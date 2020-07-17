ad21939c15228813bd01690549eaa20a0c193636
cole <cole.strine@empeq.co>
2020-07-17 17:54:42 -0400

    UI/Manual Control updated

--- UI/greenhouse/manual/views.py
+++ UI/greenhouse/manual/views.py
@@ -13,17 +13,29 @@ def index(request):
 	humidities = ''
 	soil_moistures = ''
 	sunlights = ''
+	water_actions = ''
+	fan_actions = ''
+	heat_actions = ''
+	light_actions = ''
 	for record in log_data:
 		labels += str(record) + ','
 		temperatures += str(log_data[record]['temperature']) + ','
 		humidities += str(log_data[record]['humidity']) + ','
 		soil_moistures += str(log_data[record]['soil_moisture']) + ','
 		sunlights += str(log_data[record]['sunlight']) + ','
+		water_actions += str(log_data[record]['water_action']) + ','
+		fan_actions += str(log_data[record]['fan_action']) + ','
+		heat_actions += str(log_data[record]['heat_action']) + ','
+		light_actions += str(log_data[record]['light_action']) + ','
 
 	temperatures = temperatures[:-1]
 	humidities = humidities[:-1]
 	soil_moistures = soil_moistures[:-1]
 	sunlights = sunlights[:-1]
+	water_actions = water_actions[:-1]
+	fan_actions = fan_actions[:-1]
+	heat_actions = heat_actions[:-1]
+	light_actions = light_actions[:-1]
 	labels = labels[:-1]
 
 	log_data = data_handler.get_log_data()
@@ -41,4 +53,4 @@ def index(request):
 
 	action_form = ActionForm()
 
-	return render(request, 'Manual/manual.html', {'legend': legend, 'last_temperature': last_temperature, 'last_humidity': last_humidity, 'last_soil_moisture': last_soil_moisture, 'last_sunlight': last_sunlight, 'last_reading_datetime': last_reading_datetime, 'labels': labels, 'temperatures': temperatures, 'humidities': humidities, 'soil_moistures': soil_moistures, 'sunlights': sunlights, 'action_form': action_form})
+	return render(request, 'Manual/manual.html', {'water_actions': water_actions, 'fan_actions': fan_actions, 'heat_actions': heat_actions, 'light_actions': light_actions, 'legend': legend, 'last_temperature': last_temperature, 'last_humidity': last_humidity, 'last_soil_moisture': last_soil_moisture, 'last_sunlight': last_sunlight, 'last_reading_datetime': last_reading_datetime, 'labels': labels, 'temperatures': temperatures, 'humidities': humidities, 'soil_moistures': soil_moistures, 'sunlights': sunlights, 'action_form': action_form})
