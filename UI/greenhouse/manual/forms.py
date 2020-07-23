53a6b18c39fb76bc11c179098a1d7d090f506d41
cole <cole.strine@empeq.co>
2020-07-23 13:13:06 -0400

    Built mode form on settings page, integrated with interface files, added dynamic display of ml/manual forms on settings page

--- UI/greenhouse/manual/forms.py
+++ UI/greenhouse/manual/forms.py
@@ -1,9 +1,1 @@
 from django import forms
-from scripts.data_handler import data_handler
-
-class ActionForm(forms.Form):
-	actions = [('0', '0'), ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')]
-	water = forms.ChoiceField(required=True, choices=actions, label="Water", widget=forms.Select(attrs={'onchange': 'action_form.submit();'}))
-	fan = forms.ChoiceField(required=True, choices=actions, label="Fan", widget=forms.Select(attrs={'onchange': 'action_form.submit();'}))
-	heat = forms.ChoiceField(required=True, choices=actions, label="Heat", widget=forms.Select(attrs={'onchange': 'action_form.submit();'}))
-	light = forms.ChoiceField(required=True, choices=actions, label="Light", widget=forms.Select(attrs={'onchange': 'action_form.submit();'}))
